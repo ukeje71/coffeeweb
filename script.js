8f7daeeb0542e6480ca2acebd97b17209aae3c12
ukeje isaac <ukejeisaac71@gmail.com>
2025-02-05 13:52:12 +0630

    trying Hambuger menu for tabs and phone

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuBar = document.getElementById("menu-bar");
-    const navList = document.querySelector(".nav-link");
+const hamburger = document.querySelector(".menu-bar");
+const navMenu = document.querySelector(".link-list");
 
-    menuBar.addEventListener("click", function () {
-        navList.classList.toggle("active");
-    });
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle("active");
+    hamburger.classList.toggle("active");
 });
