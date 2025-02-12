375dba63c2cdb0d48328392b9e8daa32577ee2cc
ukeje isaac <ukejeisaac71@gmail.com>
2025-02-12 11:01:57 +0630

    Applied prettier

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector(".menu-bar");
 const navMenu = document.querySelector(".link-list");
 
-hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle("active");
-    hamburger.classList.toggle("active");
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
 });
