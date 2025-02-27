42ce7b45b28c6ee3ec5843b11dc0d1a04bf2bf8d
ukeje isaac <ukejeisaac71@gmail.com>
2025-02-27 13:18:59 +0630

    Added slider to Testimonial

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
   hamburger.classList.toggle("active");
 });
+
+var swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  spaceBetween:20,
+  autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+  },
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+  navigation: { 
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+
+//Responsive Breakpoints
+breakpoints:{
+  0:{
+    slidesPerView: 1
+  },
+
+  658:{
+  slidesPerView: 2
+},
+
+1004:{
+  slidesPerView:3
+},
+}
+});
+
